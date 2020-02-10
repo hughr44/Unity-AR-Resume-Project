5873fa4be16edc9a4eaf7366dfa3f24d2550d4fa
hughr44 <39559723+hughr44@users.noreply.github.com>
2020-02-10 17:08:35 -0600

    Commit from desktop

---
+++ AR Resume/Assets/ObjectSpawner1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner1 : MonoBehaviour
+{
+    public int spawnNum;
+    public GameObject objectToSpawn1;
+    public GameObject objectToSpawn2;
+    public GameObject objectToSpawn3;
+    private PlacementIndicator placementIndicator;
+
+    void Start() {
+        spawnNum = 1;
+        placementIndicator = FindObjectOfType<PlacementIndicator>();
+    }
+
+    void Update() { 
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            if ((spawnNum + 2) % 3 == 0)
+            {
+                GameObject obj = Instantiate(objectToSpawn1, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            }
+            if ((spawnNum + 1) % 3 == 0)
+            {
+                GameObject obj = Instantiate(objectToSpawn2, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            }
+            if ((spawnNum) % 3 == 0)
+            {
+                GameObject obj = Instantiate(objectToSpawn3, placementIndicator.transform.position, placementIndicator.transform.rotation);
+            }
+            spawnNum++;
+        }
+    }
+}
---
+++ AR Resume/Assets/PlacementIndicator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class PlacementIndicator : MonoBehaviour
+{
+    private ARRaycastManager rayManager;
+    private GameObject visual;
+
+    void Start() {
+        // get components
+        rayManager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+
+        // hide placement visual
+        visual.SetActive(false);
+    }
+
+    void Update() {
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        rayManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        // check if plane hit, update position and rotation
+        if (hits.Count > 0) {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!visual.activeInHierarchy)
+                visual.SetActive(true);
+        }
+    }
+}
